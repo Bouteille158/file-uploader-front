ce67230e2506c46715063fe884a0970a4321a8d9
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-09-25 15:04:22 +0200

    feat: create file table logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,57 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../environments/environment';
+import { MatIcon } from '@angular/material/icon';
+import { FileTableComponent } from './file-table/file-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+
+  imports: [
+    RouterOutlet,
+    MatListModule,
+    MatTableModule,
+    MatIcon,
+    FileTableComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'file-uploader-front';
+  constructor(private http: HttpClient) {}
+
+  data: fileInfo[] = [];
+  displayedColumns = ['name', 'size', 'uploadDate', 'download'];
+  downloadURL = environment.apiUrl + '/download';
+
+  getData() {
+    this.http.get(environment.apiUrl + '/getAllFiles').subscribe((data) => {
+      console.log(data);
+      console.table(data);
+      if (Array.isArray(data)) {
+        this.data = data;
+      }
+    });
+
+    console.log('data is now= ' + this.data);
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+}
+
+export interface fileInfo {
+  id: string;
+  name: string;
+  size: number;
+  storageType: string;
+  type: string;
+  uploadDate: Date;
+  url: string;
 }
