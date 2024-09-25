43d8f78c9a40a109c8f7fa9cb913ced7a55ee9a9
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-09-25 15:10:12 +0200

    feat: move file table component and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatListModule } from '@angular/material/list';
-import { MatTableModule } from '@angular/material/table';
-import { HttpClient } from '@angular/common/http';
-
-import { environment } from '../environments/environment';
-import { MatIcon } from '@angular/material/icon';
 import { FileTableComponent } from './file-table/file-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [
-    RouterOutlet,
-    MatListModule,
-    MatTableModule,
-    MatIcon,
-    FileTableComponent,
-  ],
+  imports: [RouterOutlet, FileTableComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private http: HttpClient) {}
-
-  data: fileInfo[] = [];
-  displayedColumns = ['name', 'size', 'uploadDate', 'download'];
-  downloadURL = environment.apiUrl + '/download';
-
-  getData() {
-    this.http.get(environment.apiUrl + '/getAllFiles').subscribe((data) => {
-      console.table(data);
-      if (Array.isArray(data)) {
-        this.data = data;
-      }
-    });
-  }
-
-  ngOnInit() {
-    this.getData();
-  }
-}
-
-export interface fileInfo {
-  id: string;
-  name: string;
-  size: number;
-  storageType: string;
-  type: string;
-  uploadDate: Date;
-  url: string;
-}
+export class AppComponent {}
