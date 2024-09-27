ca2d616ccdcbfb80dd6a3053481ba0435ce2a8a8
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-09-27 13:10:45 +0200

    feat: open modal on upload button click

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { UploadModalComponent } from '../upload-modal/upload-modal.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-navbar',
@@ -11,4 +13,28 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(private dialog: MatDialog) {}
+
+  openUploadModal() {
+    const dialogRef = this.dialog.open(UploadModalComponent);
+    console.log(dialogRef);
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        result.subscribe(
+          (response: any) => {
+            console.log('Upload successful', response);
+            // Effectuez d'autres actions nécessaires après le succès de l'upload
+            //TODO: refresh tableau si necessaire
+          },
+          (error: any) => {
+            console.error('Upload failed', error);
+          }
+        );
+      }
+    });
+  }
+
+  ngOnInit() {}
+}
