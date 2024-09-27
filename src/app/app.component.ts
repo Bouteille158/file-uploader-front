09c70e19b75802d3b6e4c151454a423a8e060f28
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-09-27 10:47:55 +0200

    feat: add navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FileTableComponent } from './file-table/file-table.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [RouterOutlet, FileTableComponent],
+  imports: [RouterOutlet, FileTableComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
