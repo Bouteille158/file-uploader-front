475d4dcd2d6625433c645e1d9f4f6f7f97ae27e7
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-09-27 11:54:00 +0200

    feat: add upload button

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, MatIcon],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
