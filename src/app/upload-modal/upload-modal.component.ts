2ced5a5f2fb1fd9b5691272ac2ce9d1d2cf3affc
Alexandre BENALI <72262554+Bouteille158@users.noreply.github.com>
2024-10-01 21:48:00 +0200

    feat: add form validation
    max 10GB file size

--- src/app/upload-modal/upload-modal.component.ts
+++ src/app/upload-modal/upload-modal.component.ts
@@ -3,35 +3,74 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { environment } from '../../environments/environment';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Observable } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {
+  MaterialFileInputModule,
+  FileValidator,
+} from 'ngx-material-file-input';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-upload-modal',
   standalone: true,
-  imports: [MatFormFieldModule, FormsModule],
+  imports: [
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MaterialFileInputModule,
+    MatIcon,
+    ReactiveFormsModule,
+  ],
   templateUrl: './upload-modal.component.html',
   styleUrl: './upload-modal.component.scss',
 })
 export class UploadModalComponent {
   selectedFile: File | null = null;
+  form: FormGroup;
+  maxSize = 10737418241;
 
   constructor(
     private http: HttpClient,
-    private dialogRef: MatDialogRef<UploadModalComponent>
-  ) {}
+    private dialogRef: MatDialogRef<UploadModalComponent>,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      fileSelector: [
+        null,
+        [Validators.required, FileValidator.maxContentSize(this.maxSize)],
+      ],
+    });
+  }
 
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
-    if (input.files && input.files.length > 0) {
-      this.selectedFile = input.files[0];
-      console.log('New file selected : ');
-      console.log(this.selectedFile);
+    if (!input.files || input.files.length <= 0) {
+      console.log('Sélection invalide');
+      return;
     }
+
+    this.selectedFile = input.files[0];
+    console.log('New file selected : ');
+    console.log(this.selectedFile);
   }
 
-  onSubmit(): any {
+  sendFormData(): Observable<Object> | null {
     console.log('Start submission');
+
+    if (!this.form.valid) {
+      console.log('Form is not valid');
+      return null;
+    }
 
     if (!this.selectedFile) {
       console.log('No file selected');
@@ -49,8 +88,16 @@ export class UploadModalComponent {
     });
   }
 
-  closeDialog() {
-    const result = this.onSubmit();
-    this.dialogRef.close(result);
+  submitDialog() {
+    if (this.form.valid) {
+      const result = this.sendFormData();
+      this.dialogRef.close(result);
+    } else {
+      this.dialogRef.close();
+    }
+  }
+
+  onClose() {
+    this.dialogRef.close();
   }
 }
